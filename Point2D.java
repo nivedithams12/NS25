c4bb8dc7bfa38aa79fbe70cf37a2cae86f70511a
Niveditha M S <56990212+nivedithams12@users.noreply.github.com>
2021-10-11 17:44:24 +0530

    Add files via upload

---
+++ Point2D.java
@@ -0,0 +1,18 @@
+package com.One;
+
+public class Point2D {
+	int x,y;
+	
+	public Point2D(int xi,int yi) {
+		
+		x =xi;
+		y=yi;
+	}
+	
+	public String getdetails() {
+		
+		return "point ("+x+","+y+")";
+	}
+	
+
+}
---
+++ Testit.java
@@ -0,0 +1,13 @@
+package com.One;
+
+public class Testit {
+	
+	public static void main(String[] args) {
+		
+		Point2D p1 = new Point2D (10,11);
+		p1.getdetails();
+		
+		System.out.println("the details of x and y co-ordinates are:" +p1.getdetails());
+	}
+
+}
